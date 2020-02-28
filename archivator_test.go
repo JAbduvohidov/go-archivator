1f414487da1ea0bd58d8d53862c49e1f429b6778
JAbduvohidov <jaga.vohidov@gmail.com>
2020-02-28 23:03:42 +0500

    fixed: tests

--- archivator_test.go
+++ archivator_test.go
@@ -16,16 +16,16 @@ func Test_sequencedArchivator(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to read directory: %v", err)
 	}
-	if len(files) != 3 {
+	if len(files)-1 != 3 {
 		t.Fatalf("incorrect number of files, expected: 3, got: %d", len(files))
 	}
-	if files[0].Name() != "file1.txt.zip" {
+	if files[1].Name() != "file1.txt.zip" {
 		t.Fatalf("first element must be file1.txt.zip")
 	}
-	if files[1].Name() != "file2.txt.zip" {
+	if files[2].Name() != "file2.txt.zip" {
 		t.Fatalf("second element must be file1.txt.zip")
 	}
-	if files[2].Name() != "file3.txt.zip" {
+	if files[3].Name() != "file3.txt.zip" {
 		t.Fatalf("third element must be file1.txt.zip")
 	}
 }
@@ -38,16 +38,16 @@ func Test_concurrentArchivator(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to read directory: %v", err)
 	}
-	if len(files) != 3 {
+	if len(files)-1 != 3 {
 		t.Fatalf("incorrect number of files, expected: 3, got: %d", len(files))
 	}
-	if files[0].Name() != "file1.txt.zip" {
+	if files[1].Name() != "file1.txt.zip" {
 		t.Fatalf("first element must be file1.txt.zip")
 	}
-	if files[1].Name() != "file2.txt.zip" {
+	if files[2].Name() != "file2.txt.zip" {
 		t.Fatalf("second element must be file1.txt.zip")
 	}
-	if files[2].Name() != "file3.txt.zip" {
+	if files[3].Name() != "file3.txt.zip" {
 		t.Fatalf("third element must be file1.txt.zip")
 	}
 }
