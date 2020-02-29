46b2f1488a0b2dac9fb4626cb589bef58ec8a33f
JAbduvohidov <jaga.vohidov@gmail.com>
2020-02-29 12:36:14 +0500

    fixed: benchmark tests

--- archivator_test.go
+++ archivator_test.go
@@ -1,53 +1,61 @@
 package main
 
 import (
-	"io/ioutil"
 	"testing"
-	"time"
 )
 
-var fileNames = []string{"file1.txt", "file2.txt", "file3.txt"}
-
-func Test_sequencedArchivator(t *testing.T) {
-	sequencedArchivator(fileNames)
-
-	time.Sleep(time.Second)
-	files, err := ioutil.ReadDir(archivesPath)
-	if err != nil {
-		t.Fatalf("unable to read directory: %v", err)
-	}
-	if len(files)-1 != 3 {
-		t.Fatalf("incorrect number of files, expected: 3, got: %d", len(files))
-	}
-	if files[1].Name() != "file1.txt.zip" {
-		t.Fatalf("first element must be file1.txt.zip")
-	}
-	if files[2].Name() != "file2.txt.zip" {
-		t.Fatalf("second element must be file1.txt.zip")
-	}
-	if files[3].Name() != "file3.txt.zip" {
-		t.Fatalf("third element must be file1.txt.zip")
+func Benchmark_sequencedArchivator(b *testing.B) {
+	for i:=0; i< b.N; i++{
+		sequencedArchivator([]string{
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+		})
 	}
 }
 
-func Test_concurrentArchivator(t *testing.T) {
-	concurrentArchivator(fileNames)
-
-	time.Sleep(time.Second)
-	files, err := ioutil.ReadDir(archivesPath)
-	if err != nil {
-		t.Fatalf("unable to read directory: %v", err)
-	}
-	if len(files)-1 != 3 {
-		t.Fatalf("incorrect number of files, expected: 3, got: %d", len(files))
-	}
-	if files[1].Name() != "file1.txt.zip" {
-		t.Fatalf("first element must be file1.txt.zip")
-	}
-	if files[2].Name() != "file2.txt.zip" {
-		t.Fatalf("second element must be file1.txt.zip")
-	}
-	if files[3].Name() != "file3.txt.zip" {
-		t.Fatalf("third element must be file1.txt.zip")
+func Benchmark_concurrentArchivator(b *testing.B) {
+	for i:=0; i< b.N; i++{
+		concurrentArchivator([]string{
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+			"file1.txt",
+			"file2.txt",
+			"file3.txt",
+		})
 	}
 }
